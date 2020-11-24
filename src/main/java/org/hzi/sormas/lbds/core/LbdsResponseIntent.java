b83a480b27a23e92551f72e8895a3464a875a69b
MNT <noerdertuitje@gmail.com>
2020-11-24 13:17:44 +0100

    HSLB-120 deployment version for central

--- src/main/java/org/hzi/sormas/lbds/core/LbdsResponseIntent.java
+++ src/main/java/org/hzi/sormas/lbds/core/LbdsResponseIntent.java
@@ -11,17 +11,6 @@ public class LbdsResponseIntent extends LbdsBaseIntent {
     public static final String SORMAS_RECEIVER_COMPONENT="de.symeda.sormas.app.LbdsRecevierComponent";
     public static final ComponentName componentName = new ComponentName(SORMAS_APP_PCK, SORMAS_RECEIVER_COMPONENT);
 
-
-//    public LbdsResponseIntent(Context packageContext, java.lang.Class<?> cls, HttpContainer container) {
-//        super(packageContext, cls, container);
-//        setComponent(componentName);
-//    }
-//
-//    public LbdsResponseIntent(Context packageContext, java.lang.Class<?> cls, HttpResult result) {
-//        super(packageContext, cls, new HttpContainer(UUID.randomUUID(), result));
-//        setComponent(componentName);
-//    }
-
     public LbdsResponseIntent(HttpResult result) {
         super(new HttpContainer(UUID.randomUUID(), result));
         setComponent(componentName);
